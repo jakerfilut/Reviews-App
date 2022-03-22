0df8b8bc0b51a25133278dcde8746765fbd9c790
Jake <jakerfilut@gmail.com>
2022-03-21 23:03:51 -0600

    backend done

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -2,11 +2,10 @@ module Api
     module V1
         class ReviewsController < ApplicationController
 
+            protect_from_forgery with: :null_session
 
-
-            def create
-                review = Review.new(review_params)
-
+        def create
+            review = Review.new(review_params)
                 if review.save
                     render json: ReviewSerializer.new(review).serialized_json
                 else
@@ -14,25 +13,20 @@ module Api
             end
         end
 
-        def destory
+        def destroy
             review = Review.find(params[:id])
-
-            if review.destory
-                head :no_content
-            else
-                render json:{error: review.errors.messages}, status: 422
-        end
-    end
-
-
-
-
-
-            private
-
-            def review_params
-                params.require(:review).permit(:title, :description, :score, :airline_id)
+                if review.destroy
+                    head :no_content
+                else
+                    render json:{error: review.errors.messages}, status: 422
             end
+        end
+
+        private
+
+        def review_params
+            params.require(:review).permit(:title, :description, :score, :airline_id)
+        end
 
 
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
-  namsescape :api do
-    namsescape :v1 do
+  namespace :api do
+    namespace :v1 do
       resources :airlines, param: :slug
-      resources :reviews, only: [:create, :destory]
+      resources :reviews, only: [:create, :destroy]
     end
   end
 
